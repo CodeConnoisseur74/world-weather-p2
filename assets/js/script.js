63169f9af47cc694085b106ef83afe0225279e05
CodeConnoisseur74 <sherry.andrews@outlook.com>
2022-08-16 18:39:18 +0000

    initiate script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 let weather = {
-    "apiKey": "aeaadd7bd3ef0726b64f6538463c7613"
+    "apiKey": "aeaadd7bd3ef0726b64f6538463c7613", 
+    fetchWeather: function (City) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city + 
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+    }
 }
